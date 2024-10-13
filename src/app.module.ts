015853a53a4732bcee418c89785e4858a5ab2e48
AhmedHossam777 <ahmedhossam9226@gmail.com>
2024-10-13 22:09:41 +0300

    add all entities of the system

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { VendorsModule } from './vendors/vendors.module';
+import { ProductsModule } from './products/products.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, VendorsModule, ProductsModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
