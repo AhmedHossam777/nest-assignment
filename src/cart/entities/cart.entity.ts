e98ca60087b3e3eb5b71838b8b8e4f5674cfaa09
AhmedHossam777 <ahmedhossam9226@gmail.com>
2024-10-14 22:48:36 +0300

    feat[server] : add cart schema

--- src/cart/entities/cart.entity.ts
+++ src/cart/entities/cart.entity.ts
@@ -1,1 +1,20 @@
-export class Cart {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { Product } from '../../products/entities/product.entity';
+
+@Schema({
+	timestamps: true,
+	toJSON: {
+		virtuals: true,
+	},
+	toObject: {
+		virtuals: true,
+	},
+})
+export class Cart {
+	@Prop({ type: Types.ObjectId, ref: Product.name, required: true })
+	products: Types.ObjectId[] | Product[];
+
+	@Prop({ required: true })
+	quantity: number;
+}
